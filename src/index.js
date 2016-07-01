50607f848a0c08ac5fa85bbe2eedca997e84a603
troglo <manuel.martin-vivaldi@intelygenz.com>
2016-07-01 18:07:26 +0200

    Heartbeat
    Heartbeat implemented (with test)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 'use strict';
 
 const server = require('./server'),
+    heartbeat = require('./heartbeat.js'),
     createCheckout = require('./checkout/rest/createCheckout.js'),
     retrieveCheckout = require('./checkout/rest/retrieveCheckout.js');
 
 let serverInstance = server.create();
 
 server.start(serverInstance, 3000).tap(() => {
+    serverInstance.get('/api/heartbeat', heartbeat);
     serverInstance.post('/api/checkouts', createCheckout);
     serverInstance.get('/api/checkouts/:checkoutId', retrieveCheckout);
 
-    console.log('Up and running');
+    console.log('Up and running wild!');
 });
